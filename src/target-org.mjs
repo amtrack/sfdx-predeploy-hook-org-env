c53a5809ed4de6eaa820f6726b37aaabac66acc8
Matthias Rolke <mr.amtrack@gmail.com>
2023-04-24 16:12:14 +0200

    fix: parsing new --target-org flag

--- src/target-org.mjs
+++ src/target-org.mjs
@@ -4,6 +4,9 @@ import { Org } from "@salesforce/core";
 export async function getTargetOrgFromHook(hookOptions) {
   // dynamically parse the flags for the source deploy/push/delete commands
   const { flags } = await Parser.parse(hookOptions.argv, hookOptions.Command);
+  if (flags["target-org"] && typeof flags["target-org"] !== "string") {
+    return flags["target-org"];
+  }
   const aliasOrUsername = flags["target-org"] ?? flags.targetusername;
   const org = aliasOrUsername
     ? // use the command line flag if given
